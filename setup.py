df490be5c6e3adb4b8fdc508f138d7662a0ce941
Tong Zhang <zhangt@frib.msu.edu>
2018-02-09 15:14:01 -0500

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+import glob
+
+
+app_scripts = [i for i in glob.glob("scripts/*")]
+
+setup(
+        name='pyunicorn',
+        version='0.1.0',
+        description='Python client interface to Unicorn service.',
+        author='Tong Zhang',
+        author_email='zhangt@frib.msu.edu',
+        packages=['unicorn.client',
+                  'unicorn'],
+        package_dir={
+            'unicorn.client': 'unicorn/client',
+            'unicorn': 'unicorn'
+        },
+        scripts=app_scripts,
+        classifiers=[
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3.4',
+            'Topic :: Software Development :: Libraries :: Python Modules', 
+            'Topic :: Scientific/Engineering :: Physics'],
+)
---
+++ unicorn/client/utils.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from requests.adapters import HTTPAdapter
+
+
+class MyAdapter(HTTPAdapter):
+    pass
+    #def init_poolmanager(self, connections, maxsize, block=False, **kws):
+    #    self.pool
+
+
+def make_response(r):
+    if r.ok:
+        return r.json()
+    else:
+        #r.raise_for_status()
+        return {'status': r.ok, 'code': r.status_code}
+
