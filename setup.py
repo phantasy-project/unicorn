05ca5833d3b4c99afc198b7b7acdccb4130f773c
Tong Zhang <zhangt@frib.msu.edu>
2021-08-11 14:34:20 -0400

    MNT: Bump version to 0.4.2.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ app_scripts = [i for i in glob.glob("scripts/*")]
 
 setup(
         name='python-unicorn',
-        version='0.4.1',
+        version='0.4.2',
         description='Python client interface to Unicorn service.',
         author='Tong Zhang',
         author_email='zhangt@frib.msu.edu',
